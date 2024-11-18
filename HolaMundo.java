e243c5c62630014c264fa7711501570e47fbb43f
JuanDavid98S <juan.david.sanchez.cl@gmail.com>
2024-11-18 15:34:54 -0500

    Hola Mundo en Java

---
+++ HolaMundo.java
@@ -0,0 +1,14 @@
+/*
+ Hola mundo en Java
+*/
+
+    
+
+public class HolaMundo {
+
+    //Metodo main para que el archivo empiece
+
+    public static void main(String args[]) {
+        System.out.println("Hola Mundo");
+    }
+}
